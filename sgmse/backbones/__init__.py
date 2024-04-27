a67ce6f4a9db3f1b407d8a28a7c8aabee50722a8
TakumiHirose11 <taku.h.113@gmail.com>
2024-04-27 21:46:32 +0900

    feat: implement first NCSNDiT

--- sgmse/backbones/__init__.py
+++ sgmse/backbones/__init__.py
@@ -1,5 +1,6 @@
 from .shared import BackboneRegistry
 from .ncsnpp import NCSNpp
 from .dcunet import DCUNet
+from .ncsn_dit import NCSNDiTmid
 
-__all__ = ['BackboneRegistry', 'NCSNpp', 'DCUNet']
+__all__ = ['BackboneRegistry', 'NCSNpp', 'DCUNet', 'NCSNDiTmid']
